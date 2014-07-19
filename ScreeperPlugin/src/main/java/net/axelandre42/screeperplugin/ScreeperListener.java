6958efd763562b33d0ae89fb9dabdae302feff2f
Alexandre Waeles <axelandre42@yahoo.fr>
2014-07-19 20:00:39 +0200

    ScreeperPluginUtils: creating the class. -> adding httpPostRequest() method. ScreeperListener: removing things. -> adding httpPostRequest() method to onJoin() method. -> adding logging if the MalformedURLException is catched.

--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
@@ -2,6 +2,7 @@ package net.axelandre42.screeperplugin;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,12 +16,12 @@ public class ScreeperListener implements Listener {
 			this.url = new URL(url);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
-			
+			plugin.getLogger().log(Level.WARNING, "The comunicating URL is malformed ! You will can't communicate with the web interface.");
 		}
 	}
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-		
+		ScreeperPluginUtils.httpPostRequest(url, "join", e.getPlayer().getUniqueId().toString());
 	}
 }
---
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPluginUtils.java
@@ -0,0 +1,12 @@
+package net.axelandre42.screeperplugin;
+
+import java.net.URL;
+
+public class ScreeperPluginUtils {
+	public static String httpPostRequest(URL url, String... data) {
+		
+		
+		
+		return null;
+	}
+}
