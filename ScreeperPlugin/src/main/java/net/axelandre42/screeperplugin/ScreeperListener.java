d514399f8f6d1d88e7a6d408abc09fcfaacb3070
Alexandre Waeles <axelandre42@yahoo.fr>
2014-07-19 22:54:04 +0200

    I did not want to say what happened on that commit !

--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
@@ -1,5 +1,6 @@
 package net.axelandre42.screeperplugin;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
@@ -10,18 +11,26 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ScreeperListener implements Listener {
 	private URL url;
+	private ScreeperPlugin plugin;
 	
 	public ScreeperListener(String url, ScreeperPlugin plugin) {
 		try {
 			this.url = new URL(url);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
-			plugin.getLogger().log(Level.WARNING, "The comunicating URL is malformed ! You will can't communicate with the web interface.");
+			this.plugin.getLogger().log(Level.WARNING, "The comunicating URL is malformed ! You will can't communicate with the web interface.");
 		}
 	}
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
-		ScreeperPluginUtils.httpPostRequest(url, "join", e.getPlayer().getUniqueId().toString());
+		try {
+			int code = ScreeperPluginUtils.httpPostRequest(url,
+					ScreeperPluginUtils.encodeToURL("type=joinEvent", "uuid=" + e.getPlayer().getUniqueId()));
+			this.plugin.getLogger().log(Level.INFO, "The request " + url.toString() + " has returned " + code);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			this.plugin.getLogger().log(Level.WARNING, "The request to " + url.toString() + " has throwed an exception !");
+		}
 	}
 }
--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPluginUtils.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPluginUtils.java
@@ -1,12 +1,64 @@
 package net.axelandre42.screeperplugin;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class ScreeperPluginUtils {
-	public static String httpPostRequest(URL url, String... data) {
+	/**
+	 * 
+	 * @param url : The URL to post data.
+	 * @param postData : The data to post.
+	 * @return The response returned by the URL.
+	 * @throws IOException
+	 */
+	public static int httpPostRequest(URL url, String postData) throws IOException {
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		
+		// Configuring request.
+		connection.setRequestMethod("POST");
+		connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		connection.setRequestProperty("Content-Length", Integer.toString(postData.getBytes().length));
+		connection.setRequestProperty("Content-Language", "en-US");
 		
+		connection.setUseCaches(false);
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
 		
-		return null;
+		// Making request.
+		DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+		out.writeBytes(postData);
+		out.flush();
+		out.close();
+		
+		//Getting response code.
+		int code = connection.getResponseCode();
+		
+		return code;
+	}
+	/**
+	 * 
+	 * @param data : data to encode.
+	 * @return the data encoded to URL.
+	 */
+	public static String encodeToURL(String... data) {
+		StringBuffer encData = new StringBuffer();
+		for (String s : data) {
+			try {
+				encData.append(URLEncoder.encode(s, "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			encData.append("&");
+		}
+		encData.deleteCharAt(encData.length());
+		return encData.toString();
 	}
 }
