1d9bc27b2429d9afbd64c280434771be50870755
Alexandre Waeles <axelandre42@yahoo.fr>
2014-07-20 20:27:40 +0200

    Less me with your commits...

--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
@@ -27,7 +27,7 @@ public class ScreeperListener implements Listener {
 	public void onJoin(PlayerJoinEvent e) {
 		try {
 			int code = ScreeperPluginUtils.httpPostRequest(url,
-					ScreeperPluginUtils.encodeToURL("type=join", "uuid=" + e.getPlayer().getUniqueId()));
+					ScreeperPluginUtils.encodeToURL("type=join", "dName=" + e.getPlayer().getDisplayName()));
 			this.plugin.getLogger().log(Level.INFO, "The request " + url.toString() + " has returned " + code);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPlugin.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPlugin.java
@@ -2,7 +2,6 @@ package net.axelandre42.screeperplugin;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ScreeperPlugin extends JavaPlugin {
@@ -10,13 +9,9 @@ public class ScreeperPlugin extends JavaPlugin {
 	public void onEnable() {
 		this.getLogger().info("Enabling Screeper...");
 		
-		FileConfiguration config = this.getConfig();
-		
-		if (!config.contains("web.url"))
-			config.addDefault("web.url", "http://example.com/");
-		
-		this.getServer().getPluginManager().registerEvents(new ScreeperListener("", this), this);
+		this.getServer().getPluginManager().registerEvents(new ScreeperListener(this.getConfig().getString("web.url"), this), this);
 		this.getLogger().info("Screeper Enabled !");
+		
 	}
 	
 	@Override
@@ -27,7 +22,17 @@ public class ScreeperPlugin extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("screeper")) {
-			
+			if (args[0].equalsIgnoreCase("config") && sender.hasPermission("screeper.config")) {
+				if (args.length == 3) {
+					this.getConfig().set(args[1], args[2]);
+					this.saveConfig();
+					sender.sendMessage("Successfully set this configuration node !");
+					return true;
+				} else {
+					sender.sendMessage("Arguments length is not valid.");
+					return false;
+				}
+			}
 		}
 		return false;
 	}
