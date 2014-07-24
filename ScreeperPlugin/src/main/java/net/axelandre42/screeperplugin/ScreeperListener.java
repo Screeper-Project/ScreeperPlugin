509b1e7cd97a47c20a6a9a99eac2e25a4055c747
Alexandre Waeles <axelandre42@yahoo.fr>
2014-07-24 20:38:41 +0200

    A very small bugfix...

--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
@@ -20,7 +20,7 @@ public class ScreeperListener implements Listener {
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		try {
-			URL inst = new URL(url + e.getPlayer().getDisplayName());
+			URL inst = new URL(url + e.getPlayer().getName());
 			int code = ScreeperPluginUtils.httpPostRequest(inst);
 			this.plugin.getLogger().log(Level.INFO, "The request " + url.toString() + " has returned " + code);
 		} catch (IOException e1) {
