3bb3815ebe86f282ac767c9d6c24ebea906237f9
Alexandre Waeles <axelandre42@yahoo.fr>
2014-07-24 20:37:08 +0200

    Too many many many changes, bug fixes, and other things !

--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
@@ -1,7 +1,6 @@
 package net.axelandre42.screeperplugin;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 
@@ -10,40 +9,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class ScreeperListener implements Listener {
-	private URL url;
+	private String url;
 	private ScreeperPlugin plugin;
 	
 	public ScreeperListener(String url, ScreeperPlugin plugin) {
-		try {
-			this.url = new URL(url);
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			this.plugin.getLogger().log(Level.WARNING, "The comunicating URL is malformed ! You will can't communicate with the web interface.");
-		}
+		this.plugin = plugin;
+		this.url = url;
 	}
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent e) {
 		try {
-			int code = ScreeperPluginUtils.httpPostRequest(url,
-					ScreeperPluginUtils.encodeToURL("dName=" + e.getPlayer().getDisplayName()));
+			URL inst = new URL(url + e.getPlayer().getDisplayName());
+			int code = ScreeperPluginUtils.httpPostRequest(inst);
 			this.plugin.getLogger().log(Level.INFO, "The request " + url.toString() + " has returned " + code);
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			this.plugin.getLogger().log(Level.WARNING, "The request to " + url.toString() + " has throwed an exception !");
 		}
 	}
-	/*
-	@EventHandler
-	public void onQuit(PlayerQuitEvent e) {
-		try {
-			int code = ScreeperPluginUtils.httpPostRequest(url,
-					ScreeperPluginUtils.encodeToURL("type=quit", "dName=" + e.getPlayer().getDisplayName()));
-			this.plugin.getLogger().log(Level.INFO, "The request " + url.toString() + " has returned " + code);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			this.plugin.getLogger().log(Level.WARNING, "The request to " + url.toString() + " has throwed an exception !");
-		}
-	}
-	*/
 }
--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPlugin.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperPlugin.java
@@ -1,15 +1,17 @@
 package net.axelandre42.screeperplugin;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ScreeperPlugin extends JavaPlugin {
+	public ScreeperPlugin() {}
+	
 	@Override
 	public void onEnable() {
 		this.getLogger().info("Enabling Screeper...");
-		
-		this.getServer().getPluginManager().registerEvents(new ScreeperListener(this.getConfig().getString("web.url"), this), this);
+		this.getLogger().info("Getting configs");
+		String url = this.getConfig().getString("url");
+		this.saveConfig();
+		this.getServer().getPluginManager().registerEvents(new ScreeperListener(url, this), this);
 		this.getLogger().info("Screeper Enabled !");
 		
 	}
@@ -17,14 +19,5 @@ public class ScreeperPlugin extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		this.getLogger().info("Disabling Screeper...");
-	}
-	
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("scurl") && sender.hasPermission("screeper.url")) {
-			this.getConfig().set("web.url", args[0]);
-			return true;
-		}
-		return false;
 	}
 }
