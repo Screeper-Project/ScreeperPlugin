d7be3e59133c55c79450840356d4f1101b9886d7
Alexandre Waeles <axelandre42@yahoo.fr>
2014-08-21 10:48:20 +0200

    Rewriting a function

--- ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
+++ ScreeperPlugin/src/main/java/net/axelandre42/screeperplugin/ScreeperListener.java
@@ -1,9 +1,11 @@
 package net.axelandre42.screeperplugin;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Date;
 
@@ -23,17 +25,19 @@ public class ScreeperListener implements Listener {
 		File df = this.plugin.getDataFolder();
 		File jf = new File(df.getAbsolutePath()+File.separator+"connects.txt");
 		OutputStreamWriter osw = null;
+		InputStreamReader isr = null;
 		try {
 			osw = new OutputStreamWriter(new FileOutputStream(jf));
+			isr = new InputStreamReader(new FileInputStream(jf));
 			byte[] addr = e.getPlayer().getAddress().getAddress().getAddress();
 			String str = new String(new Date().getTime() + ":" + e.getPlayer().getUniqueId().toString() + ":" + e.getPlayer().getName() + ":" + addr[0] + "." + addr[1] + "." + addr[2] + "." + addr[3] + "\r");
-			char[] ch = str.toCharArray();
-			for (int i = 0; i > ch.length; i++) {
-				osw.append(ch[i]);
-				osw.flush();
+			char[] chTab = str.toCharArray();
+			for (int i = 0; i < chTab.length; i++) {
+				osw.append(chTab[i]);
 			}
-			
+			osw.flush();
 			plugin.getLogger().info(str);
+			plugin.getLogger().info("Cette ligne a été enregistré dans le fichier : " + jf.getAbsolutePath());
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
